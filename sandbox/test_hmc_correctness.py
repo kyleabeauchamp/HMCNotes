e3e19aa002259278f662bf2cdfb3990f0ad293f0
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-05-03 13:11:38 -0400

    UPdate

---
+++ sandbox/test_hmc_correctness.py
@@ -0,0 +1,110 @@
+import itertools
+import pandas as pd
+import lb_loader
+import simtk.openmm.app as app
+import numpy as np
+import simtk.openmm as mm
+from simtk import unit as u
+from openmmtools import integrators, testsystems
+
+collision_rate = 10000.0 / u.picoseconds
+temperature = 300. * u.kelvin
+
+testsystem = testsystems.LennardJonesCluster(2, 2, 2)
+#testsystem = testsystems.LennardJonesGrid()
+#testsystem = testsystems.HarmonicOscillator()
+#testsystem = testsystems.SodiumChlorideCrystal()
+#testsystem = testsystems.WaterBox(box_edge=2.25 * u.nanometers)
+
+system, positions = testsystem.system, testsystem.positions
+integrators.guess_force_groups(system, nonbonded=0, fft=0, others=0)
+
+integrator = mm.LangevinIntegrator(temperature, 1.0 / u.picoseconds, 0.25 * u.femtoseconds)
+context = mm.Context(system, integrator)
+context.setPositions(positions)
+context.setVelocitiesToTemperature(temperature)
+integrator.step(75000)
+positions = context.getState(getPositions=True).getPositions()
+
+
+def sample(context, n_iter=10, n_steps=1):
+    integrator = context.getIntegrator()
+    data = []
+    for i in itertools.count():
+        integrator.step(n_steps)
+        if type(integrator) in ["XHMCIntegrator", "XHMCRESPAIntegrator"]:
+            if integrator.getGlobalVariableByName("a") != 1.:
+                continue
+        state = context.getState(getEnergy=True)
+        energy = state.getPotentialEnergy() / u.kilojoules_per_mole
+        data.append(dict(energy=energy))
+        if len(data) >= n_iter:
+            return data
+
+
+timestep = 75 * u.femtoseconds  # LJ Cluster
+n_iter = 100000
+data = {}
+
+integrator = mm.LangevinIntegrator(temperature, 1.0 / u.picoseconds, timestep)
+context = mm.Context(system, integrator)
+context.setPositions(positions)
+context.setVelocitiesToTemperature(temperature)
+
+data["langevin"] = pd.DataFrame(sample(context, n_iter=n_iter, n_steps=20))
+
+
+integrator = integrators.GHMCIntegrator(temperature, steps_per_hmc=10, timestep=timestep, collision_rate=collision_rate)
+context = mm.Context(system, integrator)
+context.setPositions(positions)
+context.setVelocitiesToTemperature(temperature)
+
+
+data["ghmc"] = pd.DataFrame(sample(context, n_iter=n_iter, n_steps=1))
+
+
+integrator = integrators.GHMCIntegrator(temperature, steps_per_hmc=10, timestep=timestep, collision_rate=1.0 / u.picoseconds)
+context = mm.Context(system, integrator)
+context.setPositions(positions)
+context.setVelocitiesToTemperature(temperature)
+
+
+data["ghmc1"] = pd.DataFrame(sample(context, n_iter=n_iter, n_steps=1))
+
+
+integrator = integrators.XHMCIntegrator(temperature, steps_per_hmc=10, timestep=timestep, collision_rate=1.0 / u.picoseconds, extra_chances=5)
+context = mm.Context(system, integrator)
+context.setPositions(positions)
+context.setVelocitiesToTemperature(temperature)
+
+data["xhmc1"] = pd.DataFrame(sample(context, n_iter=n_iter, n_steps=1))
+
+
+
+
+groups = [(0, 2), (1, 1)]
+integrators.guess_force_groups(system, nonbonded=0, fft=1, others=0)
+integrator = integrators.GHMCRESPA(temperature, steps_per_hmc=10, timestep=timestep, collision_rate=1.0 / u.picoseconds, groups=groups)
+context = mm.Context(system, integrator)
+context.setPositions(positions)
+context.setVelocitiesToTemperature(temperature)
+
+data["ghmcrespa1"] = pd.DataFrame(sample(context, n_iter=n_iter, n_steps=1))
+
+
+groups = [(0, 2), (1, 1)]
+integrators.guess_force_groups(system, nonbonded=0, fft=1, others=0)
+integrator = integrators.XHMCRESPAIntegrator(temperature, steps_per_hmc=10, timestep=timestep, collision_rate=1.0 / u.picoseconds, extra_chances=5, groups=groups)
+context = mm.Context(system, integrator)
+context.setPositions(positions)
+context.setVelocitiesToTemperature(temperature)
+
+data["xhmcrespa1"] = pd.DataFrame(sample(context, n_iter=n_iter, n_steps=1))
+
+
+data = pd.Panel(data)
+print(testsystem)
+print(data.mean(1))
+print(data.std(1))
+
+#data.iloc[:, :, 0].plot()
---
+++ sandbox/test_hmc_correctness_results.py
@@ -0,0 +1,31 @@
+
+
+"""
+NaCl:
+              ghmc       ghmc1  ghmcrespa1    langevin       xhmc1  xhmcrespa1
+energy -604.653366 -604.669998 -604.610446 -604.613879 -604.540331 -604.688084
+           ghmc     ghmc1  ghmcrespa1  langevin     xhmc1  xhmcrespa1
+energy  3.14186  3.378239    3.498819  3.240088  3.502619    3.532189
+
+
+TiP3P
+                ghmc         ghmc1    ghmcrespa1      langevin         xhmc1     xhmcrespa1  
+energy -14562.158955 -14554.655133 -14553.599493 -14562.668393 -14534.464461    -14543.242625  
+
+              ghmc       ghmc1  ghmcrespa1    langevin       xhmc1  xhmcrespa1
+energy  115.082763  297.025921    215.0963  139.690056  260.249675  254.893465
+
+
+
+
+
+              ghmc      ghmc1  ghmcrespa1    langevin       xhmc1  xhmcrespa1
+energy -159.227422 -159.56093  -159.48125 -160.313794 -155.525069 -156.788993
+             ghmc      ghmc1  ghmcrespa1   langevin       xhmc1  xhmcrespa1
+energy  11.444632  43.674697   44.041163  10.979536  119.131399  194.117053
+
+In [2]: testsystem
+Out[2]: <openmmtools.testsystems.LennardJonesGrid at 0x7fe4b1762350>
+
+
+"""
