df4fadb034d937da425ba894bda0d39ed5a39508
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-05-08 18:12:54 -0400

    Add mixed precision

--- sandbox/misc/test_custom_particle.py
+++ sandbox/misc/test_custom_particle.py
@@ -10,7 +10,7 @@ testsystem = testsystems.CustomPotentialTestSystem()
 system, positions = testsystem.system, testsystem.positions
 
 temperature = 300 * u.kelvin
-timestep = 50.0 * u.femtoseconds
+timestep = 400.0 * u.femtoseconds
 
 integrator = integrators.HMCIntegrator(temperature, steps_per_hmc=25, timestep=timestep)
 context = lb_loader.build(system, integrator, positions, temperature)
@@ -20,3 +20,5 @@ output = integrator.vstep(10)
 data, start, g, Neff, mu, sigma, stderr = lb_loader.converge(context, Neff_cutoff=1E5)
 
 E0 = (3/2.) * testsystem.n_particles * testsystems.kB * temperature / u.kilojoules_per_mole
+
+mu, E0, mu - E0, (mu - E0) / E0
